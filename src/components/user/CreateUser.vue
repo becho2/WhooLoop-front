6de276ed472bcaebba55c6c36ed79f32ff836b9d
jinhoe <jinhoe.kim@imweb.me>
2023-05-06 13:39:56 +0900

    feat: 가입시 비밀번호 확인절차 추가

--- src/components/user/CreateUser.vue
+++ src/components/user/CreateUser.vue
@@ -25,6 +25,17 @@
             placeholder="Enter password"
           />
         </div>
+        <div>
+          <label for="password_confirm"> Password Again </label>
+          <input
+            type="password"
+            id="password_confirm"
+            v-model="passwordConfirmation"
+            name="password_confirm"
+            class="form-control"
+            placeholder="Enter password again"
+          />
+        </div>
         <div class="form-group col-md-4 pull-right">
           <button class="btn btn-success" type="submit">Create User</button>
         </div>
@@ -41,19 +52,24 @@ export default {
     return {
       email: "",
       password: "",
+      passwordConfirmation: "",
     };
   },
   methods: {
     createUser() {
+      if (this.password !== this.passwordConfirmation) {
+        alert("동일한 비밀번호를 입력해주세요.");
+        return false;
+      }
       let userData = {
         email: this.email,
         password: this.password,
       };
       this.submitToServer(userData);
     },
-    submitToServer(data) {
+    submitToServer(userData) {
       axios
-        .post(`${server.baseUrl}/user`, data)
+        .post(`${server.baseUrl}/user`, userData)
         .then((data) => {})
         .catch((error) => {
           alert(error.response.data.message);
