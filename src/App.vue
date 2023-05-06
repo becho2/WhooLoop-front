46c20b4fbccec484984f5ab0c3c6278a8e17aaa7
jinhoe <jinhoe.kim@imweb.me>
2023-05-07 07:27:56 +0900

    feat: Home에 login창 구현 api에 보내서 응답 잘 돌아오는 것까지 확인

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <router-link to="/">Home</router-link> |
   <router-link to="/section">Section</router-link>
   <router-view></router-view>
-  <div>
-    <div>후잉 바로가기</div>
+  <div class="go-to-whooing">
     <a href="https://whooing.com" target="_blank">
+      <div>후잉 바로가기</div>
       <img
         src="./assets/whooing-icon-180x180.svg"
         class="logo"
@@ -15,9 +15,12 @@
 </template>
 
 <style scoped>
+.go-to-whooing {
+  padding: 1.5em;
+}
 .logo {
   height: 6em;
-  padding: 1.5em;
+  padding: 1em;
   will-change: filter;
   transition: filter 300ms;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,4 +2,67 @@
   <div class="home">
     <h1>Home</h1>
   </div>
+  <div>
+    <div class="col-md-12 form-wrapper">
+      <form id="login-post-form" @submit.prevent="login">
+        <div class="form-group col-md-12">
+          <label for="email"> Email </label>
+          <input
+            type="text"
+            id="email"
+            v-model="email"
+            name="email"
+            class="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div>
+          <label for="password"> Password </label>
+          <input
+            type="password"
+            id="password"
+            v-model="password"
+            name="password"
+            class="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div class="form-group col-md-4 pull-right">
+          <button class="btn btn-success" type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import axios from "axios";
+import { server } from "../helper";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      let userData = {
+        email: this.email,
+        password: this.password,
+      };
+      this.submitToServer(userData);
+    },
+    submitToServer(userData: any) {
+      axios
+        .post(`${server.baseUrl}/auth/login`, userData)
+        .then((data) => {
+          this.$router.push("/section");
+        })
+        .catch((error) => {
+          alert(error.response.data.message);
+        });
+    },
+  },
+};
+</script>
