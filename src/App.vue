f4141bb58e7f7783defe29c4800ed2c0311576aa
jinhoe <jinhoe.kim@imweb.me>
2023-05-06 10:32:13 +0900

    feat: set router, sample files, add whooing logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://whooing.com" target="_blank">
+      <img
+        src="./assets/whooing-icon-180x180.svg"
+        class="logo"
+        alt="Whooing logo"
+      />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <router-link to="/">Home</router-link> |
+  <router-link to="/About">About</router-link>
+  <router-view></router-view>
 </template>
 
 <style scoped>
