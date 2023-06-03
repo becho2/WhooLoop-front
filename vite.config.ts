b4f2ca0c329b9cd8b1f6acc6d0dc463ee1cdf584
jinhoe <jinhoe.kim@imweb.me>
2023-06-03 17:53:07 +0900

    fix: CORS 에러 fix 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": {
+      "/auth": {
         //#1. axios라이브러리 등으로 http 요청인데 api로 시작하면,
         target: "https://localhost:3000", //#2. 이쪽 주소로 매핑하여 백그라운드로 보내라.
         changeOrigin: true,
