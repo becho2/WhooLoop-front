27b7ce80f91c7867245233f6d14b8358e23a13a2
jinhoe <jinhoe.kim@imweb.me>
2023-06-03 18:25:37 +0900

    fix: config file roll back

--- vite.config.ts
+++ vite.config.ts
@@ -10,16 +10,4 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      "/auth/login": {
-        //#1. axios라이브러리 등으로 http 요청인데 api로 시작하면,
-        target: "https://localhost:3000", //#2. 이쪽 주소로 매핑하여 백그라운드로 보내라.
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/auth\/login/, ""),
-        secure: false,
-        ws: true,
-      },
-    },
-  },
 });
