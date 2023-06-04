dffab5a314ac141de3111147fe0fa0bfda9b531e
jinhoe <jinhoe.kim@imweb.me>
2023-06-04 16:49:45 +0900

    fix: vite-plugin-mkcert 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), mkcert()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
-  server: {
-    https: true,
   },
 });
