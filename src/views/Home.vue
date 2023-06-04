2977958688d610607bdebeb65f17efe6cbe912e0
jinhoe <jinhoe.kim@imweb.me>
2023-06-04 22:44:06 +0900

    feat: 앱 설명 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <h2>Whooing Everyday</h2>
+    <section>
+      후잉 웹훅을 이용한 데일리/위클리 동일 거래 자동 반복입력기
+    </section>
   </div>
   <div>
     <div class="col-md-12 form-wrapper">
