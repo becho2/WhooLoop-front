5fba0162b0e6f02af234b268249a9a06affbf59a
jinhoe <jinhoe.kim@imweb.me>
2023-06-08 08:38:06 +0900

    fix: 라우터에서 삭제된 createUser경로 제거

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,6 @@ import Home from "../views/Home.vue";
 import Section from "../views/Section.vue";
 import Transaction from "../views/Transaction.vue";
 import HelloWorld from "../components/HelloWorld.vue";
-import CreateUser from "../views/user/CreateUser.vue";
 import EditUser from "../views/user/EditUser.vue";
 
 const routes = [
@@ -12,7 +11,6 @@ const routes = [
   { path: "/sections", name: "Section", component: Section },
   { path: "/transactions", name: "Transaction", component: Transaction },
   { path: "/hello", name: "HelloWorld", component: HelloWorld },
-  { path: "/create-user", name: "CreateUser", component: CreateUser },
   { path: "/edit-user", name: "EditUser", component: EditUser },
 ];
 
