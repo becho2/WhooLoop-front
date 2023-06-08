70b88f931a5dce5243c0e558805c7931a637f74f
jinhoe <jinhoe.kim@imweb.me>
2023-06-08 09:26:45 +0900

    feat: Oauth로그인으로 변경됐으므로 editUser페이지도 제거

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,14 +4,12 @@ import Home from "../views/Home.vue";
 import Section from "../views/Section.vue";
 import Transaction from "../views/Transaction.vue";
 import HelloWorld from "../components/HelloWorld.vue";
-import EditUser from "../views/user/EditUser.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/sections", name: "Section", component: Section },
   { path: "/transactions", name: "Transaction", component: Transaction },
   { path: "/hello", name: "HelloWorld", component: HelloWorld },
-  { path: "/edit-user", name: "EditUser", component: EditUser },
 ];
 
 const router = createRouter({
