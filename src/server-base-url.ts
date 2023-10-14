6a04fbb4029bfb80a4962b78da230295c95ce7e8
jinhoe <jinhoe.kim@imweb.me>
2023-10-14 16:42:02 +0900

    fix: baseUrl https 적용 url로 변경

--- src/server-base-url.ts
+++ src/server-base-url.ts
@@ -1,4 +1,5 @@
 export const server = {
   // baseUrl: "http://localhost:3000", // local dev
-  baseUrl: "http://146.56.136.6:3000", // production
+  // baseUrl: "http://146.56.136.6:3000", // production
+  baseUrl: "https://api.whooloop.todakbank.org", // production (https)
 };
